9820f716c32c653a1ad71a0ca75bbf744db25194
Mongreezy <gamefreak148d@gmail.com>
2026-06-30 13:37:00 +0200

    feat: implement onboarding greeting bot with hobby input

---
+++ 1.greetingBot.py
@@ -0,0 +1,10 @@
+# 1. The Onboarding Greeting Bot
+
+print("Welcome to the server setup!")
+username = input("What is your username?\n")
+print("Welcome to the community, " + username + "! We are glad you are here.")
+
+hobby = input("Would you like to tell us your hobby?\n")
+
+print(username + " likes " + hobby + ".")
+
